858f30c56723f235819f4e8341e7a2b80c84b2b7
jjuuuunnii <jake991110@naver.com>
2023-11-14 21:38:14 +0900

    Feat: UserEventStatusDto 추가

--- src/main/java/com/mooko/dev/controller/EventController.java
+++ src/main/java/com/mooko/dev/controller/EventController.java
@@ -24,8 +24,10 @@ public class EventController {
 
     private final AggregationFacade aggregationFacade;
 
+    @PostMapping("/my-event")
+    public ResponseEntity
     @PostMapping("/new-event")
-    public ResponseEntity<Void> makeNewEvent(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody NewEventDto newEventDto){
+    public ResponseEntity<Void> makeNewEvent(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody NewEventDto newEventDto) {
         User user = principalDetails.getUser();
         aggregationFacade.makeNewEvent(user, newEventDto);
         return ResponseEntity.status(HttpStatus.OK).build();
