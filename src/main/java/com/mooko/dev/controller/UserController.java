756f0a090603e51d0b239eb2faecc083fb656a3f
jjuuuunnii <jake991110@naver.com>
2023-11-14 22:24:55 +0900

    Refactor: 3. 나의 이벤트 api 주석달기

--- src/main/java/com/mooko/dev/controller/UserController.java
+++ src/main/java/com/mooko/dev/controller/UserController.java
@@ -4,12 +4,9 @@ import com.mooko.dev.domain.PrincipalDetails;
 import com.mooko.dev.domain.User;
 import com.mooko.dev.dto.user.res.UserEventStatusDto;
 import com.mooko.dev.facade.AggregationFacade;
-import com.mooko.dev.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +18,7 @@ public class UserController {
 
     private final AggregationFacade aggregationFacade;
 
-
+    //3. 나의 이벤트
     @GetMapping("/my-event")
     public ResponseEntity<UserEventStatusDto> showUserEventStatus(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         User user = principalDetails.getUser();
