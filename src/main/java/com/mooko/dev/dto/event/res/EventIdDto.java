f349790dfae85afaecb0ca2c8e3036bf17eb46fd
jjuuuunnii <jake991110@naver.com>
2023-11-17 16:21:07 +0900

    Fix: 3-A 이벤트 생성 api dto 추가

---
+++ src/main/java/com/mooko/dev/dto/event/res/EventIdDto.java
@@ -0,0 +1,12 @@
+package com.mooko.dev.dto.event.res;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class EventIdDto {
+    private String eventId;
+}
