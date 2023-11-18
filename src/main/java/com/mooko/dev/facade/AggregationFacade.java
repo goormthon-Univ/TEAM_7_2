06d38ecdb6da936ac3ad9b633355664f5c8bad1f
givemecow <927kmss@naver.com>
2023-11-18 16:30:56 +0900

    Refactor: 안쓰는 코드 주석처리

--- src/main/java/com/mooko/dev/facade/AggregationFacade.java
+++ src/main/java/com/mooko/dev/facade/AggregationFacade.java
@@ -16,8 +16,6 @@ import com.mooko.dev.dto.event.res.*;
 import com.mooko.dev.dto.event.socket.UserEventCheckStatusDto;
 import com.mooko.dev.dto.user.req.UserNewInfoDto;
 import com.mooko.dev.dto.user.res.UserPassportDto;
-import com.mooko.dev.event.ButtonEvent;
-import com.mooko.dev.event.LeaveEvent;
 import com.mooko.dev.exception.custom.CustomException;
 import com.mooko.dev.exception.custom.ErrorCode;
 import com.mooko.dev.service.*;
@@ -81,24 +79,23 @@ public class AggregationFacade {
 
 
 
-    private UserInfoDto createUserInfoDto(User eventUser, Event event) {
-        List<EventPhoto> eventPhotoList = eventPhotoService.findUserEventPhotoList(eventUser, event);
-        List<String> eventPhotoUrlList = eventPhotoList.stream()
-                .map(EventPhoto::getUrl)
-                .collect(Collectors.toList());
-
-        if (eventPhotoUrlList.isEmpty()) {
-            return null;
-        }
-
-        return UserInfoDto.builder()
-                .userId(eventUser.getId().toString())
-                .nickname(eventUser.getNickname())
-                .imageUrlList(eventPhotoUrlList)
-                .checkStatus(eventUser.getCheckStatus())
-                .imageCount(eventPhotoUrlList.size())
-                .build();
-    }
+//    private UserInfoDto createUserInfoDto(User eventUser, Event event) {
+//        List<EventPhoto> eventPhotoList = eventPhotoService.findUserEventPhotoList(eventUser, event);
+//        List<String> eventPhotoUrlList = eventPhotoList.stream()
+//                .map(EventPhoto::getUrl)
+//                .collect(Collectors.toList());
+//
+//        if (eventPhotoUrlList.isEmpty()) {
+//            return null;
+//        }
+//
+//        return UserInfoDto.builder()
+//                .userId(eventUser.getId().toString())
+//                .nickname(eventUser.getNickname())
+//                .imageUrlList(eventPhotoUrlList)
+//                .imageCount(eventPhotoUrlList.size())
+//                .build();
+//    }
 
 
 
@@ -143,10 +140,6 @@ public class AggregationFacade {
         }
     }
 
-
-
-
-
     //showEventBlock
     public EventPhotoResDto showEventBlock(Long eventId) {
         Event event = eventService.findEvent(eventId);
@@ -175,29 +168,29 @@ public class AggregationFacade {
      */
 
     //updateUserEventCheckStatus
-    public UserEventCheckStatusDto updateUserEventCheckStatus(UserEventCheckStatusDto userEventCheckStatusDto, Long eventId) {
-        User user = userService.findUser(Long.parseLong(userEventCheckStatusDto.getUserId()));
-        userService.updateCheckStatus(user, userEventCheckStatusDto.isCheckStatus());
-        Event event = eventService.findEvent(eventId);
-        checkEventButtonStatus(event);
-        return UserEventCheckStatusDto.builder()
-                .checkStatus(user.getCheckStatus())
-                .userId(user.getId().toString())
-                .build();
-    }
+//    public UserEventCheckStatusDto updateUserEventCheckStatus(UserEventCheckStatusDto userEventCheckStatusDto, Long eventId) {
+//        User user = userService.findUser(Long.parseLong(userEventCheckStatusDto.getUserId()));
+//        userService.updateCheckStatus(user, userEventCheckStatusDto.isCheckStatus());
+//        Event event = eventService.findEvent(eventId);
+//        checkEventButtonStatus(event);
+//        return UserEventCheckStatusDto.builder()
+//                .checkStatus(user.getCheckStatus())
+//                .userId(user.getId().toString())
+//                .build();
+//    }
 
     //바코드 생성버튼 이벤트처리
-    private void checkEventButtonStatus(Event event) {
-        List<User> userByEvent = userService.findUserByEvent(event);
-        boolean allUsersChecked = userByEvent.stream()
-                .allMatch(User::getCheckStatus);
-        log.info("바코드 생성버튼 상태입니다 id = {}, status = {} ", event.getId(), allUsersChecked);
-        eventPublisher.publishEvent(
-                ButtonEvent.builder()
-                        .buttonStatus(allUsersChecked)
-                        .eventId(event.getId().toString())
-                        .build());
-    }
+//    private void checkEventButtonStatus(Event event) {
+//        List<User> userByEvent = userService.findUserByEvent(event);
+//        boolean allUsersChecked = userByEvent.stream()
+//                .allMatch(User::getCheckStatus);
+//        log.info("바코드 생성버튼 상태입니다 id = {}, status = {} ", event.getId(), allUsersChecked);
+//        eventPublisher.publishEvent(
+//                ButtonEvent.builder()
+//                        .buttonStatus(allUsersChecked)
+//                        .eventId(event.getId().toString())
+//                        .build());
+//    }
 
 
     /**
@@ -598,7 +591,7 @@ public class AggregationFacade {
     }
 
     // showTicketInfo(quest-ticket)
-    public TicketDto showTicketInfoGuest(Long barcodeId){
+    public TicketDto showTicketInfo(Long barcodeId){
         Barcode barcode = barcodeService.findBarcode(barcodeId);
         User user = userBarcodeService.findUserBarcodeByBarcode(barcode)
                 .stream()
